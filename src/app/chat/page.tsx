2465a6a138f12cbe4dcf3990f58a77509d2e132b
luojinghui2 <luojinghui@xylink.com>
2024-06-10 17:17:36 +0800

    feat: init project

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+      <div>Chat Page</div>
+      <Link href="/">Home</Link>
+    </main>
+  );
+}
---
+++ src/components/music/index.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+// import { useState } from 'react';
+
+export default function Music() {
+  // const [number, setNumber] = useState(0);
+
+  return (
+    <div className="fixed top-0 left-0">
+      <div>music：{Math.random()}</div>
+    </div>
+  );
+}
