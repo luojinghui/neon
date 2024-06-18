ab071b541bb6c03e1be0e108f763b9408a387d10
luojinghui2 <luojinghui@xylink.com>
2024-06-18 21:26:34 +0800

    feat: add mongodb lib and connect function

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,10 +1,14 @@
+import { connectDB } from '@/server/models/index';
+
+connectDB();
+
 export async function GET(request: Request) {
   return Response.json({
     message: 'ok',
     state: 200,
     data: {
       user: '111',
-      id: 123,
-    },
+      id: 123
+    }
   });
 }
---
+++ src/server/models/index.js
@@ -0,0 +1,52 @@
+/**
+ * long description for the file
+ *
+ * @summary short description for the file
+ * @author jinghui-Luo
+ *
+ * Created at     : 2021-04-07 16:48:07
+ * Last modified  : 2024-06-18 21:19:53
+ */
+
+const mongoose = require('mongoose');
+const { db } = require('../../../config');
+const DB_HOST = db;
+
+/**
+ * 连接数据库
+ */
+const connectDB = () => {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(DB_HOST, { authSource: 'admin' });
+
+    // user是数据库名称
+    mongoose.connection.on('connected', function () {
+      console.log('MongoDB connected.');
+
+      resolve(true);
+    });
+
+    mongoose.connection.on('error', function () {
+      console.log('MongoDB failed.');
+
+      reject(false);
+    });
+
+    mongoose.connection.on('disconnected', function (e) {
+      console.log('MongoDB connected disconnected.', e);
+
+      reject(false);
+    });
+  });
+};
+
+const disconnectDB = async () => {
+  mongoose.connection.removeAllListeners();
+
+  await mongoose.disconnect();
+};
+
+module.exports = {
+  connectDB,
+  disconnectDB
+};
