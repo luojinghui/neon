ba1f3323e3af325073914befa54943099ca6c00b
luojinghui2 <luojinghui@xylink.com>
2024-07-04 13:55:20 +0800

    feat: modify home page style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,16 +38,6 @@ export default function Home() {
     setRandom(Math.round(Math.random() * 20));
   }, []);
 
-  const container = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: {
-        delayChildren: 0.2
-      }
-    }
-  };
-
   const item = {
     hidden: { opacity: 0 },
     show: { opacity: 1 }
@@ -55,17 +45,13 @@ export default function Home() {
 
   return (
     <div className="h-screen bg12 overflow-hidden">
-      <Container relative mx-auto center>
-        <div>
-          <Image src="/source/logo.svg" className="absolute left-5 top-5" draggable={false} alt="logo" width={58} height={58}></Image>
-        </div>
-      </Container>
+      <Image src="/source/logo.svg" className="fixed left-5 top-5" draggable={false} alt="logo" width={58} height={58}></Image>
 
-      <Container h-full relative className="">
-        <div className="absolute left-3 h-full">
-          <motion.ul className="h-full center flex-col" variants={container} initial="hidden" animate="show">
+      <Container className="h-full relative">
+        <div className="absolute left-3 top-0 h-full">
+          <motion.ul className="h-full center flex-col" initial="hidden" animate="show">
             <motion.li variants={item} className="group relative flex p-4 pl-6 pr-14 cursor-pointer transition rounded-md hover:bg-blue-400/70">
-              <HomeFilled className="text-white text-2xl opacity-90" />
+              <HomeFilled className="text-red-400 text-2xl opacity-90" />
               <span className="hidden text-sm  text-slate-50 absolute left-14 top-[18px] group-hover:block">首页</span>
             </motion.li>
             <motion.li variants={item} className="group relative flex p-4 pl-6 pr-14 cursor-pointer transition rounded-md hover:bg-blue-400/70">
@@ -87,8 +73,8 @@ export default function Home() {
           </motion.ul>
         </div>
 
-        <Box>
-          <div className="mt-20 relative">
+        <Box className="pt-20">
+          <div className=" relative">
             <motion.div initial={{ opacity: 0 }} animate={{ translateY: 12, scale: 1, opacity: 1 }} transition={{ duration: 1 }} className="text-6xl text-white font-medium">
               Neon Planet
             </motion.div>
@@ -103,9 +89,9 @@ export default function Home() {
 
             <motion.div
               className="absolute right-0 top-0 w-[260px]"
-              initial={{ opacity: 0, x: 80, y: -40, scale: 0.8 }}
+              initial={{ opacity: 0, x: 60, y: -30, scale: 0.8 }}
               whileInView={{ opacity: 1, x: 0, y: 0, scale: 1 }}
-              transition={{ duration: 0.5, ease: 'easeInOut', delay: 1 }}
+              transition={{ duration: 0.5, ease: 'easeInOut', delay: 0.6 }}
             >
               <img src="/source/index.png" alt="Index" className="w-full h-full" />
             </motion.div>
@@ -125,8 +111,8 @@ export default function Home() {
         </Box>
       </Container>
 
-      <Container full fixed bottom-0 center>
-        <Container mx-auto center>
+      <Container className="w-full fixed bottom-0 center">
+        <Container className="mx-auto center">
           <div className="center flex-col text-xs text-slate-50 opacity-50">
             <span className="">© {new Date().getFullYear()} Neon Planet-霓彩星球 - By JingHui</span>
             <a href="https://beian.miit.gov.cn/" rel="noopener noreferrer" className="" target="_blank">
