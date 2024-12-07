31f85b4e31920d04b7abfaf86176e57a95ca2db6
luojinghui2 <luojinghui@xylink.com>
2024-12-07 22:54:47 +0800

    feat: upgrad next v15

--- src/components/music/index.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image';
-
-export default function Music() {
-  return (
-    <div className="fixed top-0 left-0">
-      <div>music：{Math.ceil(Math.random() * 10000)}</div>
-    </div>
-  );
-}
